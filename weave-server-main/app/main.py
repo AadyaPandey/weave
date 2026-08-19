691e406106fd431658a083f5d5f05a460ab12dae
AadyaPandey <aadyapandey2004@gmail.com>
2026-08-20 01:21:26 +0530

    deploy

--- weave-server-main/app/main.py
+++ weave-server-main/app/main.py
@@ -27,7 +27,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://weeave-app.vercel.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
